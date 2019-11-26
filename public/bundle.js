b0dd18d0d9fadb38c653ae3f34959843600bb281
Victor Ljungblad <victor.ljungblad@gmail.com>
2019-11-26 13:53:21 +0100

    Added download button and text to section 3

--- public/bundle.js
+++ public/bundle.js
@@ -41,19 +41,5 @@ function showSlides(n) {
 }
 
 
-// Parallax function 
-
-// $(document).ready(function() {
-//   var onScroll = function() {
-//     var scrollTop = $(this).scrollTop();
-//     $('.section-4').each(function(index, elem) {
-//       var $elem = $(elem);
-//       $elem.find('img').css('top', scrollTop - $elem.offset().top);
-//     });
-//   };
-//   onScroll.apply(window);
-//   $(window).on('scroll', onScroll);
-// });
-
 
 
